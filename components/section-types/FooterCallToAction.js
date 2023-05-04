53ce4383469752457c1276e36d0f7f9eb6984426
Angelo Mendoza <angelo@modiphy.net>
2023-05-04 23:27:27 +0800

    Create FooterCTA

--- components/section-types/FooterCallToAction.js
+++ components/section-types/FooterCallToAction.js
@@ -1,48 +1,211 @@
-import Button from "../Button";
-
 export default function FooterCallToAction({ title, subtitle, buttons }) {
   return (
-    <section className="relative bg-transparent" id="discuss-your-project">
-      <div className="absolute bottom-0 left-0 right-0 bg-gray-900">
-        <svg
-          className="relative -top-px scale-y-flipped transform fill-current text-white"
-          viewBox="0 0 1000 50"
-        >
-          <path d="M 0 0 L 0 50 L 1000 50 L 1000 0 L 950 48 L 50 48" />
-          <path
-            d="M 16 0 L 55 38 L 945 38 L 984 0"
-            vectorEffect="non-scaling-stroke"
-            fillOpacity="0"
-            strokeWidth="4"
-            className="stroke-current text-gray-700"
-          />
-        </svg>
-      </div>
-
-      <div className="container relative z-1 max-w-screen-md pb-4 pt-8 text-center md:pb-6 md:pt-12 lg:pt-16 xl:pt-20">
-        <div className="rounded-sm bg-primary-700 bg-gradient-to-br from-primary-600 to-primary-800 p-6 shadow-lg md:p-8 lg:p-12">
-          {title && (
-            <h2 className="font-heading text-3xl font-bold text-white md:text-4xl">
-              {title}
+    <div class="relative isolate border-t border-primary-200 bg-white">
+      <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+        <div class="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
+          <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+            <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-primary-50 ring-1 ring-primary-900/10 lg:w-1/2"></div>
+            <h1 class="text-base font-semibold uppercase tracking-wide text-primary-600 md:text-lg">
+              Get in Touch
+            </h1>
+            <h2 class="mt-1 font-heading text-2xl font-normal text-gray-900 md:text-3xl xl:text-4xl">
+              Conact our dental office today!
             </h2>
-          )}
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+              Please complete our contact form and one of our patient
+              coordinators will get back to you in a jiffy.
+            </p>
+            <dl class="mt-10 space-y-4 text-base leading-7 text-gray-600">
+              <div class="flex gap-x-4">
+                <dt class="flex-none">
+                  <span class="sr-only">Address</span>
+                  <svg
+                    class="h-7 w-6 text-primary-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+                    />
+                  </svg>
+                </dt>
+                <dd>
+                  6509 Government St, Ste A <br />
+                  Baton Rouge, LA 70806
+                </dd>
+              </div>
 
-          {buttons && (
-            <div className="mt-8">
-              <div className="space-y-2 md:space-x-2 md:space-y-0">
-                {buttons?.map((button) => (
-                  <Button
-                    className="button button-base inline-flex w-full border-2 border-white bg-transparent text-center text-white hover:border-white hover:bg-white hover:text-primary-700 md:w-auto"
-                    text={button.fields.text}
-                    pageRef={button.fields.page}
-                    key={button.fields.text}
+              <div class="flex gap-x-4">
+                <dt class="flex-none">
+                  <span class="sr-only">Telephone</span>
+                  <svg
+                    class="h-7 w-6 text-primary-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                    />
+                  </svg>
+                </dt>
+                <dd>
+                  <a class="hover:text-gray-900" href="tel:(225) 924-1824">
+                    (225) 924-1824
+                  </a>
+                </dd>
+              </div>
+              <div class="flex gap-x-4">
+                <dt class="flex-none">
+                  <span class="sr-only">Email</span>
+                  <svg
+                    class="h-7 w-6 text-primary-500"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                    />
+                  </svg>
+                </dt>
+                <dd>
+                  <a
+                    class="hover:text-gray-900"
+                    href="mailto:info@levyvuteradental.com"
+                  >
+                    info@levyvuteradental.com
+                  </a>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+        <form
+          action="#"
+          method="POST"
+          class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+        >
+          <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label
+                  for="first-name"
+                  class="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div class="mt-2.5">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autocomplete="given-name"
+                    class="block w-full rounded-sm border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
                   />
-                ))}
+                </div>
+              </div>
+              <div>
+                <label
+                  for="last-name"
+                  class="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Last name
+                </label>
+                <div class="mt-2.5">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autocomplete="family-name"
+                    class="block w-full rounded-sm border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div class="sm:col-span-2">
+                <label
+                  for="email"
+                  class="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div class="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autocomplete="email"
+                    class="block w-full rounded-sm border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div class="sm:col-span-2">
+                <label
+                  for="phone-number"
+                  class="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Phone number
+                </label>
+                <div class="mt-2.5">
+                  <input
+                    type="tel"
+                    name="phone-number"
+                    id="phone-number"
+                    autocomplete="tel"
+                    class="block w-full rounded-sm border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div class="sm:col-span-2">
+                <label
+                  for="message"
+                  class="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Message
+                </label>
+                <div class="mt-2.5">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows="4"
+                    class="block w-full rounded-sm border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                  ></textarea>
+                </div>
               </div>
             </div>
-          )}
-        </div>
+            <div class="mt-8 flex justify-end">
+              <button
+                type="submit"
+                class="rounded-sm bg-primary-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+              >
+                Send message
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </section>
+      <div class="absolute bottom-0 left-0 right-0 z-1">
+        <svg
+          class="w-full fill-current text-gray-900"
+          viewBox="0 -50 1000 120"
+          preserveAspectRatio="none"
+        >
+          <path d="M 0 0 Q 500 100 1000 0 L 1000 100 L 0 100" />
+        </svg>
+      </div>
+    </div>
   );
 }
