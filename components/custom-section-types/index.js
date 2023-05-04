438bcfec3bf4cd7a0870a97dd2411ab3f06ac1a2
Angelo Mendoza <angelo@modiphy.net>
2023-05-04 22:53:32 +0800

    Export Instagram section

--- components/custom-section-types/index.js
+++ components/custom-section-types/index.js
@@ -2,3 +2,4 @@ export { default as HomeHero } from "./HomeHero";
 export { default as DentalServices } from "./DentalServices";
 export { default as WhyChooseUs } from "./WhyChooseUs";
 export { default as Testimonials } from "./Testimonials";
+export { default as Instagram } from "./Instagram";
