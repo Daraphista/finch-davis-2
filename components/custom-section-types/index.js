2ee4ccbab9348ccb35028b1af8be6485fbb2cb95
Angelo Mendoza <angelo@modiphy.net>
2023-05-04 22:34:20 +0800

    Export Testimonials section

--- components/custom-section-types/index.js
+++ components/custom-section-types/index.js
@@ -1,3 +1,4 @@
 export { default as HomeHero } from "./HomeHero";
 export { default as DentalServices } from "./DentalServices";
 export { default as WhyChooseUs } from "./WhyChooseUs";
+export { default as Testimonials } from "./Testimonials";
