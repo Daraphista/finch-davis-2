0066844462efc2df9dabf220ae18e9e05ae0052a
Angelo Mendoza <angelo@modiphy.net>
2023-05-05 00:14:23 +0800

    Fix Instagram element attributes

--- components/custom-section-types/Instagram.js
+++ components/custom-section-types/Instagram.js
@@ -1,75 +1,75 @@
 export default function Instagram() {
   return (
     <section>
-      <div class="container max-w-screen-xl py-12 text-center md:py-16 lg:py-20">
+      <div className="container max-w-screen-xl py-12 text-center md:py-16 lg:py-20">
         <div>
-          <h1 class="text-base font-semibold uppercase tracking-wide text-primary-600 md:text-lg">
+          <h1 className="text-base font-semibold uppercase tracking-wide text-primary-600 md:text-lg">
             Follow us on Instagram
           </h1>
-          <h2 class="mt-1 font-heading text-2xl font-normal text-gray-900 md:text-3xl xl:text-4xl">
+          <h2 className="mt-1 font-heading text-2xl font-normal text-gray-900 md:text-3xl xl:text-4xl">
             Reprehenderit ea duis nisi ad commodo
           </h2>
         </div>
-        <div class="mt-10 grid grid-cols-3 gap-4 md:grid-cols-4">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+        <div className="mt-10 grid grid-cols-3 gap-4 md:grid-cols-4">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
             />
           </div>
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
             />
           </div>
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
             />
           </div>
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
             />
           </div>
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
             />
           </div>
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
             />
           </div>
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
             />
           </div>
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
+          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
             <img
-              class="absolute left-0 top-0 h-full w-full object-cover object-center"
+              className="absolute left-0 top-0 h-full w-full object-cover object-center"
               src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
               loading="lazy"
