2b473cf57c3381b419d20a6d55b8ccff07291c88
Angelo Mendoza <angelo@modiphy.net>
2023-05-04 23:43:32 +0800

    Create FooterEmailSignup

--- components/section-types/FooterEmailSignup.js
+++ components/section-types/FooterEmailSignup.js
@@ -2,51 +2,49 @@ import Link from "next/link";
 
 export default function FooterEmailSignup({ title }) {
   return (
-    <section className="relative bg-gray-900">
-      <div className="container relative z-1 max-w-screen-xl py-8 text-left">
-        <div className="rounded-sm border-4 border-primary-600 px-6 py-6 md:px-12 md:py-12 lg:px-16 lg:py-16 xl:flex xl:items-center">
-          <div className="xl:-mt-2 xl:w-0 xl:flex-1">
-            {title && (
-              <h2 className="font-heading text-2xl font-semibold text-white md:text-4xl">
-                {title}
-              </h2>
-            )}
+    <section class="relative bg-gray-900">
+      <div class="container relative z-1 max-w-screen-xl py-8 text-left">
+        <div class="rounded-sm border-2 border-primary-500 px-6 py-6 md:px-12 md:py-12 lg:px-16 lg:py-16 xl:flex xl:items-center">
+          <div class="xl:-mt-2 xl:w-0 xl:flex-1">
+            <h2 class="font-heading text-2xl font-semibold text-white md:text-4xl">
+              Sign up to receive news and updates in your inbox
+            </h2>
           </div>
-          <div className="mt-8 sm:w-full sm:max-w-md xl:ml-8 xl:mt-0">
+          <div class="mt-8 sm:w-full sm:max-w-md xl:ml-8 xl:mt-0">
             <form
               name="form1294"
-              className="wufoo sm:flex"
-              acceptCharset="UTF-8"
-              autoComplete="off"
-              encType="multipart/form-data"
+              class="wufoo sm:flex"
+              accept-charset="UTF-8"
+              autocomplete="off"
+              enctype="multipart/form-data"
               method="post"
               action="https://modiphy.wufoo.com/forms/zzb9yb30rquiqj/#public"
             >
-              <label htmlFor="emailAddress" className="sr-only">
+              <label for="emailAddress" class="sr-only">
                 Email address
               </label>
               <input
                 id="Field6"
                 name="Field6"
-                spellCheck="false"
+                spellcheck="false"
                 type="email"
-                autoComplete="email"
-                maxLength="255"
+                autocomplete="email"
+                maxlength="255"
                 required=""
-                className="w-full rounded-sm border-gray-400 px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-700"
+                class="w-full rounded-sm border-gray-400 px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-700"
                 placeholder="Enter your email"
               />
               <button
                 id="saveForm"
                 name="saveForm"
                 type="submit"
-                className="button mt-3 flex w-full border-transparent bg-primary-600 text-white hover:border-transparent hover:bg-primary-700 hover:text-white focus:ring-white focus:ring-offset-primary-600 sm:ml-3 sm:mt-0 sm:w-auto sm:flex-shrink-0"
+                class="button mt-3 flex w-full border-transparent bg-primary-500 text-white hover:border-transparent hover:bg-primary-600 hover:text-white focus:ring-white focus:ring-offset-primary-600 sm:ml-3 sm:mt-0 sm:w-auto sm:flex-shrink-0"
               >
                 Subscribe
               </button>
 
-              <div className="hidden">
-                <label htmlFor="comment">Do Not Fill This Out</label>
+              <div class="hidden">
+                <label for="comment">Do Not Fill This Out</label>
                 <textarea
                   name="comment"
                   id="comment"
@@ -68,14 +66,15 @@ export default function FooterEmailSignup({ title }) {
               </div>
             </form>
 
-            <p className="mt-3 text-center text-sm text-gray-400">
+            <p class="mt-3 text-center text-sm text-gray-400">
               We care about the protection of your data. Read our
-              <Link
+              <a
                 href="/privacy-policy"
-                className="font-bold text-gray-300 hover:underline"
+                class="font-bold text-gray-300 hover:underline"
               >
-                <> Privacy Policy.</>
-              </Link>
+                {" "}
+                Privacy Policy.{" "}
+              </a>
             </p>
           </div>
         </div>
