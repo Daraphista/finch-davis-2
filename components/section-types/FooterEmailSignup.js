4182ba31709b45c5934eaf9a44defb0dcdcabc3a
Angelo Mendoza <angelo@modiphy.net>
2023-05-05 10:15:26 +0800

    Fix FooterEmailSignup class attributes

--- components/section-types/FooterEmailSignup.js
+++ components/section-types/FooterEmailSignup.js
@@ -2,27 +2,27 @@ import Link from "next/link";
 
 export default function FooterEmailSignup({ title }) {
   return (
-    <section class="relative bg-gray-900">
-      <div class="container relative z-1 max-w-screen-xl py-8 text-left">
-        <div class="rounded-sm border-2 border-primary-500 px-6 py-6 md:px-12 md:py-12 lg:px-16 lg:py-16 xl:flex xl:items-center">
-          <div class="xl:-mt-2 xl:w-0 xl:flex-1">
+    <section className="relative bg-gray-900">
+      <div className="container relative z-1 max-w-screen-xl py-8 text-left">
+        <div className="rounded-sm border-2 border-primary-500 px-6 py-6 md:px-12 md:py-12 lg:px-16 lg:py-16 xl:flex xl:items-center">
+          <div className="xl:-mt-2 xl:w-0 xl:flex-1">
             {title && (
-              <h2 class="font-heading text-2xl font-semibold text-white md:text-4xl">
+              <h2 className="font-heading text-2xl font-semibold text-white md:text-4xl">
                 {title}
               </h2>
             )}
           </div>
-          <div class="mt-8 sm:w-full sm:max-w-md xl:ml-8 xl:mt-0">
+          <div className="mt-8 sm:w-full sm:max-w-md xl:ml-8 xl:mt-0">
             <form
               name="form1294"
-              class="wufoo sm:flex"
+              className="wufoo sm:flex"
               acceptCharset="UTF-8"
               autoComplete="off"
               encType="multipart/form-data"
               method="post"
               action="https://modiphy.wufoo.com/forms/zzb9yb30rquiqj/#public"
             >
-              <label htmlFor="emailAddress" class="sr-only">
+              <label htmlFor="emailAddress" className="sr-only">
                 Email address
               </label>
               <input
@@ -33,19 +33,19 @@ export default function FooterEmailSignup({ title }) {
                 autoComplete="email"
                 maxLength="255"
                 required=""
-                class="w-full rounded-sm border-gray-400 px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-700"
+                className="w-full rounded-sm border-gray-400 px-5 py-3 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-700"
                 placeHolder="Enter your email"
               />
               <button
                 id="saveForm"
                 name="saveForm"
                 type="submit"
-                class="button mt-3 flex w-full border-transparent bg-primary-500 text-white hover:border-transparent hover:bg-primary-600 hover:text-white focus:ring-white focus:ring-offset-primary-600 sm:ml-3 sm:mt-0 sm:w-auto sm:flex-shrink-0"
+                className="button mt-3 flex w-full border-transparent bg-primary-500 text-white hover:border-transparent hover:bg-primary-600 hover:text-white focus:ring-white focus:ring-offset-primary-600 sm:ml-3 sm:mt-0 sm:w-auto sm:flex-shrink-0"
               >
                 Subscribe
               </button>
 
-              <div class="hidden">
+              <div className="hidden">
                 <label htmlFor="comment">Do Not Fill This Out</label>
                 <textarea
                   name="comment"
@@ -68,11 +68,11 @@ export default function FooterEmailSignup({ title }) {
               </div>
             </form>
 
-            <p class="mt-3 text-center text-sm text-gray-400">
+            <p className="mt-3 text-center text-sm text-gray-400">
               We care about the protection of your data. Read our
               <a
                 href="/privacy-policy"
-                class="font-bold text-gray-300 hover:underline"
+                className="font-bold text-gray-300 hover:underline"
               >
                 {" "}
                 Privacy Policy.{" "}
