e6f5184a56f10369a0833710426d42ef97b7808f
Angelo Mendoza <angelo@modiphy.net>
2023-05-08 21:39:00 +0800

    Change InteriorHero svgs

--- components/section-types/InteriorHero.js
+++ components/section-types/InteriorHero.js
@@ -10,7 +10,6 @@ export default function InteriorHero({ title }) {
 
       <div className="pointer-events-none absolute inset-0 overflow-hidden"></div>
 
-      <div className="relative"></div>
       <div className="container relative z-10 mx-auto max-w-screen-2xl px-8 pb-16 pt-24 md:pb-24 md:pt-32 lg:pb-40">
         <div className="flex justify-center lg:mt-8">
           {title && (
@@ -21,18 +20,51 @@ export default function InteriorHero({ title }) {
         </div>
       </div>
 
-      <div className="absolute -bottom-px left-0 right-0">
+      <div className="absolute inset-0">
         <svg
-          className="relative -top-px fill-current text-white"
-          viewBox="0 0 1000 50"
+          className="absolute bottom-0 right-0 h-[70%] w-[70%] fill-current text-primary-500"
+          viewBox="0 0 1000 1000"
+          preserveAspectRatio="none"
+          fillOpacity="0.15"
         >
-          <path d="M 0 0 L 0 50 L 1000 50 L 1000 0 L 950 48 L 50 48" />
+          <path d="M 0 1000 Q 250 250 1000 0 L 1000 1000 Z" />
+        </svg>
+        <svg
+          className="absolute bottom-0 right-0 h-3/5 w-3/5 fill-current text-primary-500"
+          viewBox="0 0 1000 1000"
+          preserveAspectRatio="none"
+          fillOpacity="0.25"
+        >
+          <path d="M 0 1000 Q 250 250 1000 0 L 1000 1000 Z" />
+        </svg>
+        <svg
+          className="absolute bottom-0 right-0 h-1/2 w-1/2 fill-current text-primary-500"
+          viewBox="0 0 1000 1000"
+          preserveAspectRatio="none"
+          fillOpacity="0.4"
+        >
+          <path d="M 0 1000 Q 250 250 1000 0 L 1000 1000 Z" />
+        </svg>
+      </div>
+
+      <div className="relative z-1">
+        <svg
+          className="w-full fill-current text-white"
+          viewBox="0 0 1000 100"
+          preserveAspectRatio="none"
+        >
+          <path d="M 0 0 Q 500 100 1000 0 L 1000 100 L 0 100" />
+        </svg>
+        <svg
+          className="strokeCurrent absolute bottom-0 left-0 right-0 text-primary-200"
+          viewBox="0 -10 1000 100"
+          preserveAspectRatio="none"
+        >
           <path
-            d="M 16 0 L 55 38 L 945 38 L 984 0"
+            d="M 0 0 Q 500 100 1000 0"
+            fill="none"
+            strokeWidth="2"
             vectorEffect="non-scaling-stroke"
-            fillOpacity="0"
-            strokeWidth="4"
-            className="strokeCurrent text-primary-600"
           />
         </svg>
       </div>
