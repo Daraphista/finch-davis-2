f5df49f835bdb2d03cf2211c952afa0ff43f0b2a
Angelo Mendoza <angelo@modiphy.net>
2023-05-08 21:48:33 +0800

    Change title container style

--- components/section-types/InteriorHero.js
+++ components/section-types/InteriorHero.js
@@ -11,7 +11,7 @@ export default function InteriorHero({ title, subtitle }) {
 
       <div className="pointer-events-none absolute inset-0 overflow-hidden"></div>
       <div className="container relative z-10 mx-auto max-w-screen-2xl px-8 pb-16 pt-24 md:pb-24 md:pt-32 lg:pb-40">
-        <div className="flex justify-center lg:mt-8">
+        <div className="text-center lg:mt-8">
           {title && (
             <h1 className="mt-3 font-heading text-4xl font-normal text-primary-950 md:text-5xl xl:text-6xl">
               {title}
