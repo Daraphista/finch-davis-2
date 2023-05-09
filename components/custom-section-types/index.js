4f9268a1d8bfaa1fd4551c214142ae9857d9a1a1
Angelo Mendoza <angelo@modiphy.net>
2023-05-09 22:35:33 +0800

    Export WhatWeDo

--- components/custom-section-types/index.js
+++ components/custom-section-types/index.js
@@ -3,3 +3,4 @@ export { default as DentalServices } from "./DentalServices";
 export { default as WhyChooseUs } from "./WhyChooseUs";
 export { default as Testimonials } from "./Testimonials";
 export { default as Instagram } from "./Instagram";
+export { default as WhatWeDo } from "./WhatWeDo";
