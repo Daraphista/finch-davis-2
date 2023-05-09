cff01c0cb572f3ddf41a99be6d81f04f0051d9ae
Angelo Mendoza <angelo@modiphy.net>
2023-05-09 23:36:46 +0800

    Render title in MeetOurDoctors

--- components/custom-section-types/MeetOurDoctors.js
+++ components/custom-section-types/MeetOurDoctors.js
@@ -1,11 +1,13 @@
-export default function MeetOurDoctors() {
+export default function MeetOurDoctors({ title }) {
   return (
     <div class="bg-white py-24 sm:py-32">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl sm:text-center">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Meet our leadership
-          </h2>
+          {title && (
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              {title}
+            </h2>
+          )}
           <p class="mt-6 text-lg leading-8 text-gray-600">
             We're a dynamic group of individuals who are passionate about what
             we do and dedicated to delivering the best results for our clients.
