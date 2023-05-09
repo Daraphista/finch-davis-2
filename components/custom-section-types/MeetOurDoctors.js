46048b0a5fa48b008d1fde135ccaa04a2869074a
Angelo Mendoza <angelo@modiphy.net>
2023-05-09 23:34:47 +0800

    Rename MeetOurTeam section

--- components/custom-section-types/MeetOurDoctors.js
+++ components/custom-section-types/MeetOurDoctors.js
@@ -1,4 +1,4 @@
-export default function MeetOurTeam() {
+export default function MeetOurDoctors() {
   return (
     <div class="bg-white py-24 sm:py-32">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
