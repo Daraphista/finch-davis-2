7f87c9b740d0a50ac0ecd8c9badd8dbf3e061712
Angelo Mendoza <angelo@modiphy.net>
2023-05-09 11:01:22 +0800

    Render footer social links

--- components/Footer.js
+++ components/Footer.js
@@ -17,7 +17,30 @@ export default async function Footer() {
 
         <div>
           <FooterLogo footerLogo={data?.footerLogo} metaData={metaData} />
+
           <ModiphyBannerFooter metaData={metaData} />
+
+          {data?.footerSocialMediaLinks && (
+            <div class="mt-5 flex justify-center space-x-5">
+              {data?.footerSocialMediaLinks?.map((socialMediaLink) => (
+                <a
+                  class="text-gray-400 hover:text-white"
+                  href="#f"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span class="sr-only">LinkedIn</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-8 w-8 fill-current"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" />
+                  </svg>
+                </a>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </footer>
