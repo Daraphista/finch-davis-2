6c9c5a860fadc740120015356d712c7047ec5265
Angelo Mendoza <angelo@modiphy.net>
2023-05-10 22:48:05 +0800

    Add color to WhyChooseUs svgs

--- components/custom-section-types/WhyChooseUs.js
+++ components/custom-section-types/WhyChooseUs.js
@@ -38,18 +38,12 @@ export default function WhyChooseUs({
                       key={`${customContentItem?.sys?.id}${uniqid()}`}
                     >
                       <dt className="inline font-semibold text-gray-900">
-                        <svg
-                          className="absolute left-1 top-1 h-5 w-5 text-primary-600"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
+                        <div
+                          className="text-primary-600"
+                          dangerouslySetInnerHTML={{
+                            __html: customContentItem?.fields?.iconSvg,
+                          }}
+                        />
                         {customContentItem?.fields?.title}{" "}
                       </dt>
                       {customContentItem?.fields?.subtitle && (
