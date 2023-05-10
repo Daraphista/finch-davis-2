d6dc9d447250d6ad3ce0819decfcaebb08cd7289
Angelo Mendoza <angelo@modiphy.net>
2023-05-10 11:57:33 +0800

    Render dentistry categories in DentalServices

--- components/custom-section-types/DentalServices.js
+++ components/custom-section-types/DentalServices.js
@@ -2,6 +2,7 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS } from "@contentful/rich-text-types";
 import Link from "next/link";
 import uniqid from "uniqid";
+import ConsoleLogger from "../ConsoleLogger";
 
 const options = {
   renderNode: {
@@ -39,42 +40,85 @@ export default function DentalServices({
         {customContentCollection && (
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-              {customContentCollection?.map((dentalService) => (
-                <div
-                  className="flex flex-col"
-                  key={`${dentalService?.sys?.id}${uniqid()}`}
-                >
-                  <dt className="text-lg font-semibold leading-7 text-gray-900">
-                    <div className="relative mb-6 flex h-10 w-10 items-center justify-center rounded-sm bg-primary-600">
-                      {dentalService?.fields?.iconSvg && (
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html: dentalService?.fields?.iconSvg,
-                          }}
-                        />
-                      )}
-                      <div className="absolute -right-1.5 -top-1.5 h-10 w-10 border-r-2 border-t-2 border-primary-300"></div>
+              {customContentCollection?.map((dentalService) => {
+
+                if (dentalService?.sys?.contentType?.sys?.id === "dentistryCategory") {
+                  return (
+                    <div
+                      className="flex flex-col lg:col-span-3"
+                      key={`${dentalService?.sys?.id}${uniqid()}`}
+                    >
+                      <dt className="text-lg font-semibold leading-7 text-gray-900">
+                        <div className="relative mb-6 flex h-10 w-10 items-center justify-center rounded-sm bg-primary-600">
+                          {dentalService?.fields?.iconSvg && (
+                            <div
+                              dangerouslySetInnerHTML={{
+                                __html: dentalService?.fields?.iconSvg,
+                              }}
+                            />
+                          )}
+                          <div className="absolute -right-1.5 -top-1.5 h-10 w-10 border-r-2 border-t-2 border-primary-300"></div>
+                        </div>
+                        {dentalService?.fields?.title}
+                      </dt>
+
+                      <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                        {documentToReactComponents(
+                          dentalService?.fields?.body,
+                          dentalServiceOptions
+                        )}
+
+                        <p className="mt-6">
+                          <Link
+                            href="/"
+                            className="text-base font-semibold leading-6 text-primary-600 hover:text-primary-900"
+                          >
+                            Learn more <span aria-hidden="true">→</span>
+                          </Link>
+                        </p>
+                      </dd>
                     </div>
-                    {dentalService?.fields?.title}
-                  </dt>
+                  );
 
-                  <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
-                    {documentToReactComponents(
-                      dentalService?.fields?.body,
-                      dentalServiceOptions
-                    )}
+                }
+                
+                return (
+                  <div
+                    className="flex flex-col"
+                    key={`${dentalService?.sys?.id}${uniqid()}`}
+                  >
+                    <dt className="text-lg font-semibold leading-7 text-gray-900">
+                      <div className="relative mb-6 flex h-10 w-10 items-center justify-center rounded-sm bg-primary-600">
+                        {dentalService?.fields?.iconSvg && (
+                          <div
+                            dangerouslySetInnerHTML={{
+                              __html: dentalService?.fields?.iconSvg,
+                            }}
+                          />
+                        )}
+                        <div className="absolute -right-1.5 -top-1.5 h-10 w-10 border-r-2 border-t-2 border-primary-300"></div>
+                      </div>
+                      {dentalService?.fields?.title}
+                    </dt>
 
-                    <p className="mt-6">
-                      <Link
-                        href="/"
-                        className="text-base font-semibold leading-6 text-primary-600 hover:text-primary-900"
-                      >
-                        Learn more <span aria-hidden="true">→</span>
-                      </Link>
-                    </p>
-                  </dd>
-                </div>
-              ))}
+                    <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                      {documentToReactComponents(
+                        dentalService?.fields?.body,
+                        dentalServiceOptions
+                      )}
+
+                      <p className="mt-6">
+                        <Link
+                          href="/"
+                          className="text-base font-semibold leading-6 text-primary-600 hover:text-primary-900"
+                        >
+                          Learn more <span aria-hidden="true">→</span>
+                        </Link>
+                      </p>
+                    </dd>
+                  </div>
+                );
+              })}
 
               <Link
                 className="group flex flex-col border-2 border-primary-200 bg-primary-50 p-6 hover:border-primary-400"
