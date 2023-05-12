71fa0926be89423fda887561c018f28e122cfd8e
Angelo Mendoza <angelo@modiphy.net>
2023-05-12 13:56:52 +0800

    Adjust dental service gap

--- components/custom-section-types/DentalServices.js
+++ components/custom-section-types/DentalServices.js
@@ -39,7 +39,7 @@ export default function DentalServices({
         </div>
         {customContentCollection && (
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
-            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-3">
               {customContentCollection?.map((dentalCategory) => {
                 if (
                   dentalCategory?.sys?.contentType?.sys?.id ===
