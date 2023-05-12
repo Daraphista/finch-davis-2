01ff9311cfe983f0230bd706720706d4aa4babbb
Angelo Mendoza <angelo@modiphy.net>
2023-05-12 13:47:15 +0800

    Remove icon from nested link

--- components/NestedLink.js
+++ components/NestedLink.js
@@ -76,21 +76,6 @@ export default function NestedLink({ navLink, isScrolling }) {
                       className="group/child-nav flex items-center border-b border-gray-200 border-opacity-50 px-5 pb-4 pt-6 hover:bg-gray-50"
                     >
                       <div className="ml-3 flex items-center gap-3 transition-all">
-                        <svg
-                          className="fill-gray-400 group-hover/child-nav:fill-primary-500"
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="far"
-                          data-icon="shield-check"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                          data-fa-i2svg=""
-                          height="16"
-                          weight="16"
-                        >
-                          <path d="M466.5 83.71l-192-80c-5.875-2.5-12.16-3.703-18.44-3.703S243.5 1.203 237.6 3.703L45.61 83.71C27.73 91.08 16 108.6 16 127.1C16 385.4 205.4 512 255.9 512C305.2 512 496 387.3 496 127.1C496 108.6 484.3 91.08 466.5 83.71zM256 464C158.5 423.4 64 297.3 64 128l192-80L448 128C448 301.8 349.6 425 256 464zM200.1 247C196.3 242.3 190.1 239.1 184 239.1c-13.71 0-24 11.21-24 24C160 270.1 162.3 276.3 167 280.1l48 48C219.5 333.5 225.7 335.1 232 335.1c2.595 0 11.46-.4962 18.22-8.375l96-112c3.881-4.528 5.781-10.09 5.781-15.62c0-7.405-5.79-23.99-23.98-23.99c-6.756 0-13.48 2.831-18.24 8.362L230.7 276.7L200.1 247z"></path>
-                        </svg>
                         <p className="text-theme-title group-hover/child-nav:text-secondary-950 font-heading text-sm font-semibold tracking-wide 2xl:text-base">
                           {navLinkButton?.fields?.title}
                         </p>
