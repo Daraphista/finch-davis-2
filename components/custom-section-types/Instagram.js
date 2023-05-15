30898fddf5cac35d9098780a6b244cc527511cb7
Angelo Mendoza <angelo@modiphy.net>
2023-05-15 11:01:49 +0800

    Remove instagram placeholder feed

--- components/custom-section-types/Instagram.js
+++ components/custom-section-types/Instagram.js
@@ -10,72 +10,7 @@ export default function Instagram() {
             Reprehenderit ea duis nisi ad commodo
           </h2>
         </div>
-        <div className="mt-10 grid grid-cols-3 gap-4 md:grid-cols-4">
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-          <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-sm shadow-md">
-            <img
-              className="absolute left-0 top-0 h-full w-full object-cover object-center"
-              src="https://images.unsplash.com/photo-1499651681375-8afc5a4db253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHNtaWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              loading="lazy"
-            />
-          </div>
-        </div>
+       
       </div>
     </section>
   );
