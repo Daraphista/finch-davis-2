44343c810715048710ddef4a3e1971961b5dac18
Angelo Mendoza <angelo@modiphy.net>
2023-05-18 21:20:34 +0800

    Render title in instagram section

--- components/custom-section-types/Instagram.js
+++ components/custom-section-types/Instagram.js
@@ -1,13 +1,15 @@
 import Script from "next/script";
 
-export default function Instagram() {
+export default function Instagram(title) {
   return (
     <section>
       <div className="container max-w-screen-xl py-12 text-center md:py-16 lg:py-20">
         <div>
-          <h1 className="text-base font-semibold uppercase tracking-wide text-primary-600 md:text-lg">
-            Follow us on Instagram
-          </h1>
+          {title && (
+            <h1 className="text-base font-semibold uppercase tracking-wide text-primary-600 md:text-lg">
+              {title}
+            </h1>
+          )}
           <h2 className="mt-1 font-heading text-2xl font-normal text-gray-900 md:text-3xl xl:text-4xl">
             Reprehenderit ea duis nisi ad commodo
           </h2>
