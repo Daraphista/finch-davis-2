c351ee8c583f643470405af66444f531a24d37bc
Angelo Mendoza <angelo@modiphy.net>
2023-05-18 21:21:49 +0800

    Render instagram subtitle

--- components/custom-section-types/Instagram.js
+++ components/custom-section-types/Instagram.js
@@ -1,6 +1,6 @@
 import Script from "next/script";
 
-export default function Instagram(title) {
+export default function Instagram(title, subtitle) {
   return (
     <section>
       <div className="container max-w-screen-xl py-12 text-center md:py-16 lg:py-20">
@@ -10,9 +10,11 @@ export default function Instagram(title) {
               {title}
             </h2>
           )}
-          <h2 className="mt-1 font-heading text-2xl font-normal text-gray-900 md:text-3xl xl:text-4xl">
-            Reprehenderit ea duis nisi ad commodo
-          </h2>
+          {subtitle && (
+            <h2 className="mt-1 font-heading text-2xl font-normal text-gray-900 md:text-3xl xl:text-4xl">
+              {subtitle}
+            </h2>
+          )}
         </div>
 
         <div className="mt-10">
