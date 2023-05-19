79c8c0d1c6468385ffebdd9ba46a4cf51922af77
Angelo Mendoza <angelo@modiphy.net>
2023-05-19 11:14:55 +0800

    Add main element in 404 page

--- pages/404.js
+++ pages/404.js
@@ -7,28 +7,30 @@ export default function Error({ headerData, metaData }) {
       {/* Temporary banner so that the header can be seen */}
       <div className="h-[95px] bg-gray-900"></div>
 
-      <section className="relative bg-transparent">
-        <div className="container relative z-1 max-w-screen-lg pb-12 pt-12 text-center md:pb-12 md:pt-12 lg:pb-16 lg:pt-16 xl:pb-20 xl:pt-20">
-          <div className="">
-            <h2
-              className="text-theme-title font-heading text-4xl font-extrabold md:text-5xl"
-              color="text-theme-title"
-            >
-              Wait a second...
-            </h2>
-            <div className="prose-bg-primary-lighter prose mx-auto mt-6 md:prose-lg">
-              We&apos;re sorry, this page doesn&apos;t exist. You can{" "}
-              <Link
-                href="/"
-                className="text-theme-title-primary font-bold text-primary-600 underline"
+      <main>
+        <section className="relative bg-transparent">
+          <div className="container relative z-1 max-w-screen-lg pb-12 pt-12 text-center md:pb-12 md:pt-12 lg:pb-16 lg:pt-16 xl:pb-20 xl:pt-20">
+            <div className="">
+              <h2
+                className="text-theme-title font-heading text-4xl font-extrabold md:text-5xl"
+                color="text-theme-title"
               >
-                explore the rest of our site.
-              </Link>
-              .
+                Wait a second...
+              </h2>
+              <div className="prose-bg-primary-lighter prose mx-auto mt-6 md:prose-lg">
+                We&apos;re sorry, this page doesn&apos;t exist. You can{" "}
+                <Link
+                  href="/"
+                  className="text-theme-title-primary font-bold text-primary-600 underline"
+                >
+                  explore the rest of our site.
+                </Link>
+                .
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </main>
     </>
   );
 }
