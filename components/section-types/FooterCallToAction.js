85d52718fea99e69dad28b931de32630bf8f4552
Angelo Mendoza <angelo@modiphy.net>
2023-06-01 00:04:07 +0800

    Increase margin top in footer cta

--- components/section-types/FooterCallToAction.js
+++ components/section-types/FooterCallToAction.js
@@ -17,7 +17,7 @@ export default function FooterCallToAction({
   customContentCollection,
 }) {
   return (
-    <div className="relative isolate border-t border-primary-200 bg-white">
+    <div className="relative isolate mt-20 border-t border-primary-200 bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
